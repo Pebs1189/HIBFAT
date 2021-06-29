6151d2c4653df4c89d513b4bbb00c427ceaca3b9
Pablo Barba <pabbarso181189@gmail.com>
2021-06-29 17:20:40 +0200

    function gets.js from tests/helper/ modified

--- tests/helper/gets.js
+++ tests/helper/gets.js
@@ -3,12 +3,12 @@ const axios = require('axios').default;
 
 //get all users - params: url(http://localhost:3000/users/)
 const getUsers = async (url) => {
-    return await (await axios.get(url)).data;
+    return (await axios.get(url)).data;
 };
 
 //get an user by id- params: url(http://localhost:3000/users/), id(60d1a2dfef5b4527f841603c)
 const getUserById = async (url, id) => {
-    return await (await axios.get(url + id)).data;
+    return (await axios.get(url + id)).data;
 };
 
 module.exports = {
