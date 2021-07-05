deb072622233801cc9f6c7ed32c52b06dfa7a0c8
Pablo Barba <pabbarso181189@gmail.com>
2021-07-05 17:08:29 +0200

    ; added

--- helpers/passwordUtils.js
+++ helpers/passwordUtils.js
@@ -7,7 +7,7 @@ const encryptPassword = (password) => {
 }
 
 const isValidPassword = (password, hash) => {
-    const res = bcryptjs.compareSync(password, hash)
+    const res = bcryptjs.compareSync(password, hash);
 
     return res;
 }
