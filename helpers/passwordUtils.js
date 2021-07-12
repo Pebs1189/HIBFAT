77f2f69118c03d027d722013ca2a1acf885d1786
Pablo Barba <pabbarso181189@gmail.com>
2021-07-12 13:56:49 +0200

    Crypto-js with new passwordUtils functions added

--- helpers/passwordUtils.js
+++ helpers/passwordUtils.js
@@ -1,16 +1,41 @@
 const bcryptjs = require('bcryptjs');
+const CryptoJS = require('crypto-js');
 
+require('dotenv').config({path:'./.env'});
+
+const secret_key = process.env.SECRET_KEY;
+const secret_key2 = process.env.SECRET_KEY2;
+
+//using bcryptjs to encrypt user password
 const encryptPassword = (password) => {
     const salt = bcryptjs.genSaltSync();
 
     return bcryptjs.hashSync(password, salt);
 }
 
+//using bcryptjs to compare if password is correct
 const isValidPassword = (password, hash) => {
     return bcryptjs.compareSync(password, hash);
 }
 
+//using crypto-js to encrypt plain text
+const encryptPlainText = (text = '') => {
+    const textEncrypted = CryptoJS.AES.encrypt(text, secret_key2).toString();
+
+    return textEncrypted;
+}
+
+//using crypto-js to decrypt plain text encrypted before
+const decryptPlainTextEncrypted = (textEncrypted) => {
+    const bytes = CryptoJS.AES.decrypt(textEncrypted, secret_key2);
+    const textOriginal = bytes.toString(CryptoJS.enc.Utf8);  
+
+    return textOriginal;
+}
+
 module.exports = {
     encryptPassword,
-    isValidPassword
+    isValidPassword,
+    encryptPlainText,
+    decryptPlainTextEncrypted
 }
